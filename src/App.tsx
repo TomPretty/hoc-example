92117ec347e9ddfb85df854764ebce52f335a725
Tom Pretty <tompretty2@gmail.com>
2021-06-17 14:32:19 +0100

    Add cache example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { Header, SupportAgainHeader, AusHeader } from './components/Header';
-import './App.css';
+import { useEffect } from "react";
+import { Header, SupportAgainHeader, AusHeader } from "./components/Header";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetchUserData("tom");
+    fetchUserData("tom");
+    fetchUserData("tom");
+    fetchUserData("ara");
+    fetchUserData("ara");
+  }, []);
+
   return (
     <div className="App">
       <Header baseUrl="https://support.theguardian.com" />
@@ -9,6 +18,27 @@ function App() {
       <AusHeader baseUrl="https://support.theguardian.com" />
     </div>
   );
+}
+
+function _fetchUserData(userName: string): string {
+  console.log(`fetching data for: ${userName}`);
+  return userName;
+}
+
+const fetchUserData = withCache(_fetchUserData);
+
+function withCache(func: (param: string) => string) {
+  const cache: Record<string, string> = {};
+
+  function wrapped(param: string) {
+    if (!(param in cache)) {
+      cache[param] = func(param);
+    }
+
+    return cache[param];
+  }
+
+  return wrapped;
 }
 
 export default App;
